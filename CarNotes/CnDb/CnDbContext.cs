a201a18f0bd9e2e718f884b5b016d8dbea79322b
Elena Gerasimovich <nararock@mail.ru>
2021-06-04 01:48:12 +0300

    add Entity Framework, add a new context class (CnDbContext)

---
+++ CarNotes/CnDb/CnDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.CnDb
+{
+    public class CnDbContext: DbContext
+    {
+        public CnDbContext() : base("name=CnDb") { }
+    }
+}

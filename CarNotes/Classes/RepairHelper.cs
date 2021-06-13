0dfc45f8e62f6153218d9a3231e7a0e9b4021986
Elena Gerasimovich <nararock@mail.ru>
2021-06-13 16:54:16 +0300

    add Repair Helper (Classes) and Repair Model(Models)

---
+++ CarNotes/Classes/RepairHelper.cs
@@ -0,0 +1,26 @@
+﻿using CarNotes.CnDb;
+using CarNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Classes
+{
+    public class RepairHelper
+    {
+        public void SaveToDataBase(RepairModel rm)
+        {
+            var database = new CnDbContext();
+            var repairEvent = new RepairEvent();
+            repairEvent.CarService = rm.CarService;
+            repairEvent.Comments = rm.Comments;
+            repairEvent.Date = rm.Date;
+            repairEvent.Mileage = rm.Mileage;
+            repairEvent.Repair = rm.Repair;
+            repairEvent.RepairCost = rm.RepairCost;
+            database.RepairEvents.Add(repairEvent);
+            database.SaveChanges();
+        }
+    }
+}
---
+++ CarNotes/Models/RepairModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class RepairModel
+    {
+        public DateTime Date { get; set; }
+        public double Mileage { get; set; }
+        public string Repair { get; set; }
+        //public List<CarPart> Parts { get; set; }
+        public string CarService { get; set; }
+        public decimal RepairCost { get; set; }
+        public string Comments { get; set; }
+    }
+}

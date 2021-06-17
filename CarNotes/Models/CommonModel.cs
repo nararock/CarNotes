4d87e054e62b43b89f6d291c8d616f56179d680a
Elena Gerasimovich <nararock@mail.ru>
2021-06-18 02:09:28 +0300

    создание модели для описания данных общей таблицы

---
+++ CarNotes/Models/CommonModel.cs
@@ -0,0 +1,16 @@
+﻿using CarNotes.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class CommonModel
+    {
+        public RecordType Record { get; set; }
+        public DateTime Date { get; set; }
+        public double Mileage { get; set; }
+        public double Cost { get; set; }
+    }
+}

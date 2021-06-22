7b4cf77f6b88cc43f7e2fee42574063b24bf40eb
Elena Gerasimovich <nararock@mail.ru>
2021-06-23 01:26:55 +0300

    добавлена старинца регистрации

---
+++ CarNotes/Classes/RegistrationHelper.cs
@@ -0,0 +1,24 @@
+﻿using CarNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Classes
+{
+    public class RegistrationHelper
+    {
+        public void Register(RegistrationModel rm, HttpContextBase context)
+        {
+            var authHelper = new AuthHelper(context);
+            var user = new CnDb.User
+            {
+                Name = rm.Name,
+                Email = rm.Email,
+                UserName = rm.Email
+            };
+            authHelper.UserManager.CreateAsync(user);
+            authHelper.UserManager.AddPasswordAsync(user.Id, rm.Password);
+        }
+    }
+}
---
+++ CarNotes/Controllers/RegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using CarNotes.Classes;
+using CarNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarNotes.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(RegistrationModel rm)
+        {
+            new RegistrationHelper().Register(rm, HttpContext);
+            return View("EndOfRegistration");
+        }
+    }
+}

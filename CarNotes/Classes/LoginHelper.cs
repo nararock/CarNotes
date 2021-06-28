a92a5279defd9dc16f3f800bb858e71ebeca0c32
Elena Gerasimovich <nararock@mail.ru>
2021-06-29 00:09:21 +0300

    добавлена страница для входа по логину и паролю, также добавлен контроллер, класс и модель для операции входа

---
+++ CarNotes/Classes/LoginHelper.cs
@@ -0,0 +1,19 @@
+﻿using CarNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CarNotes.Classes
+{
+    public class LoginHelper
+    {
+        public async Task<bool> Login(LoginModel lm, HttpContextBase context) 
+        {
+            var authHelper = new AuthHelper(context);
+            var result = await authHelper.SignInManager.PasswordSignInAsync(lm.Login, lm.Password, true, true);
+            return result == Microsoft.AspNet.Identity.Owin.SignInStatus.Success;
+        }
+    }
+}
---
+++ CarNotes/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using CarNotes.Classes;
+using CarNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarNotes.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(LoginModel loginModel)
+        {
+            if (!await new LoginHelper().Login(loginModel, HttpContext))
+            {
+                ModelState.AddModelError("", "Не верно введен логин или пароль");
+                return View();
+            }
+            return Redirect("~");
+        }
+    }
+}

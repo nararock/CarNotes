813859f7ac86f6b49fd87ac9022e09827dbdf64c
Elena Gerasimovich <nararock@mail.ru>
2021-07-04 23:13:02 +0300

    добавлен файл Main.js с функциями для работы с Cookie

---
+++ CarNotes/Content/Main.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+    var vehicleIdCookie = getCookie('vehicleId');
+    var elem = document.getElementById('vehicleSelect');
+    if (elem.children.length == 0) { return; }
+    if (vehicleIdCookie == undefined) {
+        elem.children[0].selected = true;
+        setCookie('vehicleId', elem.value);
+    }
+    else if (vehicleIdCookie != undefined) {
+        elem.value = vehicleIdCookie;
+    }
+    elem.addEventListener("change", function () {
+        setCookie('vehicleId', elem.value);
+    })
+}

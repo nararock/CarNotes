975888c51825dd133350b0f0babc3b85d4eb79f8
Elena Gerasimovich <nararock@mail.ru>
2021-07-12 01:08:16 +0300

    исправлен код класса CommonHelper: создается общая таблица для каждой машины (vehicleId)в отдельности, а не общая для всех

--- CarNotes/Classes/CommonHelper.cs
+++ CarNotes/Classes/CommonHelper.cs
@@ -9,11 +9,13 @@ namespace CarNotes.Classes
 {
     public class CommonHelper
     {
-        public List<CommonModel> CreateList()
+        public List<CommonModel> CreateList(int vehicleId)
         {
             var db = new CnDbContext();
-            var list = db.RefuelEvents.Select(x => new CommonModel {Record = Enums.RecordType.Refuel, Date = x.Date, Mileage = x.Mileage, Cost = x.PricePerOneLiter * x.Volume }).ToList();
-            list.AddRange(db.RepairEvents.Select(x => new CommonModel {Record = Enums.RecordType.Repair, Date = x.Date, Mileage = x.Mileage, Cost = (double)x.RepairCost }));
+            var vehicle = db.Vehicles.Find(vehicleId);
+            if (vehicle == null) return null;
+            var list = vehicle.RefuelEvents.Select(x => new CommonModel {Record = Enums.RecordType.Refuel, Date = x.Date, Mileage = x.Mileage, Cost = x.PricePerOneLiter * x.Volume }).ToList();
+            list.AddRange(vehicle.RepairEvents.Select(x => new CommonModel {Record = Enums.RecordType.Repair, Date = x.Date, Mileage = x.Mileage, Cost = (double)x.RepairCost }));
             list = list.OrderBy(x => x.Mileage).ToList();
             return list;
         }

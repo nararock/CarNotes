9b4673a732ff4e314bf0c079e286b704b0760524
Elena Gerasimovich <nararock@mail.ru>
2021-08-21 02:38:10 +0300

    добавлена функция ChangeData для внесения изменений в базу данных при изменении события , связанного с заправкой

--- CarNotes/Classes/RepairHelper.cs
+++ CarNotes/Classes/RepairHelper.cs
@@ -93,5 +93,46 @@ namespace CarNotes.Classes
             }
             return editRepairModel;
         }
+
+        public void ChangeData(RepairModel rm)
+        {
+            var db = new CnDbContext();
+            var repairEvent = db.RepairEvents.Include(x => x.Parts).Where(y => y.Id == rm.Id).FirstOrDefault();
+            repairEvent.CarService = rm.CarService;
+            repairEvent.Comments = rm.Comments;
+            var tempDate = new DateTime();
+            DateTime.TryParse(rm.Date, out tempDate);
+            repairEvent.Date = tempDate;
+            repairEvent.Mileage = rm.Mileage;
+            repairEvent.Repair = rm.Repair;
+            repairEvent.RepairCost = rm.RepairCost;
+            var carPartsDelete = new List<CarPart>();
+            foreach(var p in rm.Parts)
+            {
+                if (p.Id != 0 && p.IsDeleted == true)
+                {
+                    repairEvent.Parts.Remove(repairEvent.Parts.Where(x => x.Id == p.Id).FirstOrDefault());
+                } 
+                else if (p.Id != 0)
+                {
+                    var carPartDb = repairEvent.Parts.Where(x => x.Id == p.Id).FirstOrDefault();
+                    carPartDb.Article = p.Article;
+                    carPartDb.CarManufacturer = p.CarManufacturer;
+                    carPartDb.Name = p.Name;
+                    carPartDb.Price = p.Price;
+                } 
+                else if (p.Id == 0 && p.IsDeleted != true)
+                {
+                    var carParts = new CarPart();
+                    carParts.Id = -1;
+                    carParts.Name = p.Name;
+                    carParts.Price = p.Price;
+                    carParts.Article = p.Article;
+                    carParts.CarManufacturer = p.CarManufacturer;
+                    repairEvent.Parts.Add(carParts);
+                }
+            }
+            db.SaveChanges();
+        }
     }
 }

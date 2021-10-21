8091e9b650ce1f83749ca23112605350f048854e
Elena Gerasimovich <nararock@mail.ru>
2021-10-21 18:39:00 +0300

    добавление к предыдущему коммиту

--- CarNotes/CnDb/CarPart.cs
+++ CarNotes/CnDb/CarPart.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,7 @@ namespace CarNotes.CnDb
 {
     public class CarPart
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string CarManufacturer { get; set; }
@@ -15,5 +18,6 @@ namespace CarNotes.CnDb
         public int CarSubsystemId { get; set; }
         public CarSubsystem CarSubsystem { get; set; }
         [Column("RepairEvent_Id")]
+        public int RepairEventId { get; set; }
     }
 }

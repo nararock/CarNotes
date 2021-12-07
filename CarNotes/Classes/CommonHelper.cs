77d92164387f0bf30a93bc09a0b9ed0167bcd2f1
Elena Gerasimovich <nararock@mail.ru>
2021-12-08 02:03:56 +0300

    добавлено округление при расчете стоимости заправки

--- CarNotes/Classes/CommonHelper.cs
+++ CarNotes/Classes/CommonHelper.cs
@@ -14,7 +14,7 @@ namespace CarNotes.Classes
             var db = new CnDbContext();
             var vehicle = db.Vehicles.Find(vehicleId);
             if (vehicle == null) return null;
-            var list = vehicle.RefuelEvents.Select(x => new CommonModel {Id = x.ID, Record = Enums.RecordType.Refuel, Date = x.Date.ToString("dd.MM.yyyy"), Mileage = x.Mileage, Cost = x.PricePerOneLiter * x.Volume }).ToList();
+            var list = vehicle.RefuelEvents.Select(x => new CommonModel {Id = x.ID, Record = Enums.RecordType.Refuel, Date = x.Date.ToString("dd.MM.yyyy"), Mileage = x.Mileage, Cost = Math.Round(x.PricePerOneLiter * x.Volume) }).ToList();
             list.AddRange(vehicle.RepairEvents.Select(x => new CommonModel {Id = x.Id, Record = Enums.RecordType.Repair, Date = x.Date.ToString("dd.MM.yyyy"), Mileage = x.Mileage, Cost = (double)x.RepairCost }));
             list = list.OrderBy(x => x.Mileage).ToList();
             return list;

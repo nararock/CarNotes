f51d4e5eb16e04dba5f8f0edb5e18c12cda7d161
Elena Gerasimovich <nararock@mail.ru>
2021-12-30 01:23:47 +0300

    для параметра RepairCost добавлено округление в функции GetList

--- CarNotes/Classes/RepairHelper.cs
+++ CarNotes/Classes/RepairHelper.cs
@@ -17,7 +17,7 @@ namespace CarNotes.Classes
             var vehicle = db.Vehicles.Include(v=>v.RepairEvents.Select(r=>r.Parts)).FirstOrDefault(x => x.Id == vehicleId);
             if (vehicle == null) return null;
             var list = vehicle.RepairEvents.Select(x => new RepairModel {Id = x.Id, Date=x.Date.ToString("dd.MM.yyyy"), Mileage=x.Mileage, Repair=x.Repair,
-                CarService=x.CarService, RepairCost=x.RepairCost, Comments=x.Comments, Parts=x.Parts.Select(y=>new CarPartModel { Article=y.Article,
+                CarService=x.CarService, RepairCost= Math.Round(x.RepairCost), Comments=x.Comments, Parts=x.Parts.Select(y=>new CarPartModel { Article=y.Article,
                     CarManufacturer=y.CarManufacturer, Name=y.Name, Price=y.Price}).ToList() }).ToList();
             return list;
         }

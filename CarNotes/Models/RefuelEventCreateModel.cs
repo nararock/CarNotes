d1a45bd6f8c36db20f24dd20f8e209c05cd00adb
Elena Gerasimovich <nararock@mail.ru>
2022-01-31 16:59:18 +0300

    новая модель для вывода вида топлива и последнего пробега

---
+++ CarNotes/Models/RefuelEventCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using CarNotes.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class RefuelEventCreateModel
+    {
+        public double? LastMileage { get; set; }
+        public FuelType LastFuel { get; set; }
+    }
+}

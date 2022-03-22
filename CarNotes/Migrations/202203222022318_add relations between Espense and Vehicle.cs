44dcb39e62b97450a1dec83d636b021561491f6b
Elena Gerasimovich <nararock@mail.ru>
2022-03-23 01:09:08 +0300

    миграции

---
+++ CarNotes/Migrations/202203222022318_add relations between Espense and Vehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace CarNotes.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addrelationsbetweenEspenseandVehicle : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Expenses", "VehicleId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Expenses", "VehicleId");
+            AddForeignKey("dbo.Expenses", "VehicleId", "dbo.Vehicles", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Expenses", "VehicleId", "dbo.Vehicles");
+            DropIndex("dbo.Expenses", new[] { "VehicleId" });
+            DropColumn("dbo.Expenses", "VehicleId");
+        }
+    }
+}

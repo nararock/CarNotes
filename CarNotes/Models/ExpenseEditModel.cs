759223efa152f81d9ff3c8789e4dc50cc11a3dc6
Elena Gerasimovich <nararock@mail.ru>
2022-03-30 02:17:33 +0300

    1. добавлена страница ExpenseEdit.cshtml для создания нового события типа Расходы 2. добавлены модель данных ExpenseEditModel для сохранения новых событий типа Расходы  и модель TypeExpenseModel для вывода и сохранения данных о типе расхода

---
+++ CarNotes/Models/ExpenseEditModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class ExpenseEditModel
+    {
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        public string Date { get; set; }
+        public string Sum { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+    }
+}

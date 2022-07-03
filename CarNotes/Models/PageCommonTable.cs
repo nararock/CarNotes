c086d695e16e713cc43c7918cfc0881b8c02c1d3
Elena Gerasimovich <nararock@mail.ru>
2022-07-04 01:50:48 +0300

    1. Добавлена модель страницы PageContainer для постраничного вывода данных (хранение номера страницы, всего событий). 2. Добавлена модель PageCommonTable (объединяет события для общей таблицы и модель страницы)

---
+++ CarNotes/Models/PageCommonTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class PageCommonTable: PageContainer
+    {
+        public PageCommonTable(int count, int pageNumber, int pageSize): base(count, pageNumber, pageSize) { }
+        public List<CommonModel> PageList { get; set; }
+    }
+}
---
+++ CarNotes/Models/PageContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class PageContainer
+    {
+        public int PageNumber { get; private set; }
+        public int TotalPages { get; private set; }
+        public PageContainer(int count, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageNumber > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageNumber < TotalPages);
+            }
+        }
+    }
+}

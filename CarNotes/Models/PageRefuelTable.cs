83153fe56355fa65c647554d59065897988e2b0a
Elena Gerasimovich <nararock@mail.ru>
2022-07-25 22:36:59 +0300

    Добавлена новая модель данных PageRefuelTable для постраничного вывода таблицы с заправками.

---
+++ CarNotes/Models/PageRefuelTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class PageRefuelTable : PageContainer
+    {
+        public PageRefuelTable(int count, int pageNumber, int pageSize) : base(count, pageNumber, pageSize) { }
+        public List<RefuelModelOutput> PageList { get; set; }
+    }
+}

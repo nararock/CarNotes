6d3a3e93f0d1e1e5b442c601e154829ef6135be8
Elena Gerasimovich <nararock@mail.ru>
2022-07-27 02:19:40 +0300

    Добавлена новая модель PageExpenseTable

---
+++ CarNotes/Models/PageExpenseTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class PageExpenseTable: PageContainer
+    {
+        public PageExpenseTable(int count, int pageNumber, int pageSize) : base(count, pageNumber, pageSize) { }
+        public List<ExpenseModel> PageList { get; set; }
+    }
+}

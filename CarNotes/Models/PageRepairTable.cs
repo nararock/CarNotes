8ae86fb466d19b6598f77e406572857649d3f6d0
Elena Gerasimovich <nararock@mail.ru>
2022-07-26 22:07:15 +0300

    Добавлена модель данных (PageRepairTable) для постраничного вывода данных на странице с таблицей ремонтов.

---
+++ CarNotes/Models/PageRepairTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class PageRepairTable : PageContainer
+    {
+        public PageRepairTable(int count, int pageNumber, int pageSize) : base(count, pageNumber, pageSize) { }
+        public List<RepairModel> PageList { get; set; }
+    }
+}

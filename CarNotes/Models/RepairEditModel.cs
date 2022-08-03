97ca105f27d98a407ec1523d37f76dbaa2611f8b
Elena Gerasimovich <nararock@mail.ru>
2022-08-03 23:30:04 +0300

    1. Изменение типа данного Date в моделях ExpenseEditModel, RefuelModel для корректного  отображения даты в окнах редактирования и создания события. 2.Добавлена новая модель RepairEditModel (где поле Date  типа DateTime).

---
+++ CarNotes/Models/RepairEditModel.cs
@@ -0,0 +1,26 @@
+﻿using CarNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class RepairEditModel
+    {
+        public RepairEditModel()
+        {
+            Parts = new List<CarPartModel>();
+            Date = DateTime.Now.ToString("dd.MM.yyyy");
+        }
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public double Mileage { get; set; }
+        public string Repair { get; set; }
+        public List<CarPartModel> Parts { get; set; }
+        public string CarService { get; set; }
+        public double RepairCost { get; set; }
+        public string Comments { get; set; }
+        public bool WrongMileage { get; set; }
+    }
+}

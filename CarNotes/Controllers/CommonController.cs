2ea31366d53f8a48bf9fb52c63cf98b9951879c5
Elena Gerasimovich <nararock@mail.ru>
2022-08-08 22:48:31 +0300

    Внесение изменений расположения строчек в коде.

--- CarNotes/Controllers/CommonController.cs
+++ CarNotes/Controllers/CommonController.cs
@@ -15,33 +15,34 @@ namespace CarNotes.Controllers
         // GET:Common
         public ActionResult Index(int? vehicleId, int pageNumber=1)
         {
-            if (vehicleId != null)
+            if (vehicleId == null)
             {
-                ViewBag.VehicleId = vehicleId;
-                ViewBag.IsChecked = false;
-                if (HttpContext.User.Identity.IsAuthenticated)
-                {
-                    var userIdCheck = new AuthHelper(HttpContext).AuthenticationManager.User.Identity.GetUserId();
-                    if (new CnDbContext().Users.Find(userIdCheck).Vehicles.Any(v => v.Id == vehicleId))
-                    {
-                        ViewBag.IsChecked = true;
-                        HttpContext.Response.Cookies.Set(new HttpCookie("vehicleId", vehicleId.ToString()));
-                    }
-                }
-                int pageSize = 1;//количество выводимых событий на 1-ой странице 
-                var commonModelList = new CommonHelper().GetList((int)vehicleId, pageNumber, pageSize);
-                if (commonModelList == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
-                var database = new CnDbContext();
-                var countEvent = database.RefuelEvents.Where(e => e.VehicleId == vehicleId).Count()
-                    + database.RepairEvents.Where(e => e.VehicleId == vehicleId).Count() + database.Expenses.Where(e=>e.VehicleId == vehicleId).Count();                
-                var pageModel = new PageCommonTable(countEvent, pageNumber, pageSize);
-                pageModel.PageList = commonModelList;
-                ViewBag.Name = "Общая таблица";
-                return View(pageModel);
+                return Redirect("~/Home/Resolve?ReturnURL=/Common/Index");
             }
-            return Redirect("~/Home/Resolve?ReturnURL=/Common/Index");
+
+            ViewBag.VehicleId = vehicleId;
+            ViewBag.IsChecked = false;
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                var userIdCheck = new AuthHelper(HttpContext).AuthenticationManager.User.Identity.GetUserId();
+                if (new CnDbContext().Users.Find(userIdCheck).Vehicles.Any(v => v.Id == vehicleId))
+                {
+                    ViewBag.IsChecked = true;
+                    HttpContext.Response.Cookies.Set(new HttpCookie("vehicleId", vehicleId.ToString()));
+                }
+            }
+            int pageSize = 1;//количество выводимых событий на 1-ой странице 
+            var commonModelList = new CommonHelper().GetList((int)vehicleId, pageNumber, pageSize);
+            if (commonModelList == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            var database = new CnDbContext();
+            var countEvent = database.RefuelEvents.Where(e => e.VehicleId == vehicleId).Count()
+                + database.RepairEvents.Where(e => e.VehicleId == vehicleId).Count() + database.Expenses.Where(e => e.VehicleId == vehicleId).Count();
+            var pageModel = new PageCommonTable(countEvent, pageNumber, pageSize);
+            pageModel.PageList = commonModelList;
+            ViewBag.Name = "Общая таблица";
+            return View(pageModel);
         }
-           
+
         public ActionResult DeleteEvent(string record, int id)
         {
             if(record == "Refuel")

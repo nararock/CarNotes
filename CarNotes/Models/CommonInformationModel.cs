beb91fed89a1bb504dc1e8103bd6b003946a5e84
Elena Gerasimovich <nararock@mail.ru>
2022-09-10 19:51:19 +0300

    1. Добавлен класс CommonInformationModel для хранения данных тп=аблицы с общей информацией в разделе общей статистики. 2. Добавлен класс CommonTimeOnSite для хранения данных об общем времени, проведенном на сайте для таблицы с общей информацией.

---
+++ CarNotes/Models/CommonInformationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class CommonInformationModel
+    {
+        public CommonTimeOnSite TotalTime { get; set; }
+        public int CommonMileage { get; set; }
+        public int RefuelAmount { get; set; }
+        public int RefuelCost { get; set; }
+        public int RepairAmount { get; set; }
+        public int RepairCost { get; set; }
+        public int ExpenseAmount { get; set; }
+        public int ExpenseCost { get; set; }
+        public int AverageFuelPrice { get; set;}
+    }
+}
---
+++ CarNotes/Models/CommonTimeOnSite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarNotes.Models
+{
+    public class CommonTimeOnSite
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public string formYear { get; set; }
+        public string formMonth { get; set; }
+        public string formDay { get; set; } 
+    }
+}

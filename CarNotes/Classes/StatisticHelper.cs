edd9c8d075c9317e7c3acc0ef7f507c1d2423f43
Elena Gerasimovich <nararock@mail.ru>
2022-09-10 20:10:59 +0300

    1. Добавлен GetDataForCommonInformation для получения данных и вывод их в таблице на странице общей статистики. 2. Добавлен метод GetTimeSpentOnSite для вычисления общего времени ведения статистики на сайте. 3. Добавлен метод GetWordForm для формирования правильной формы слова для обозначения временного интервала в общую таблицу.

--- CarNotes/Classes/StatisticHelper.cs
+++ CarNotes/Classes/StatisticHelper.cs
@@ -3,6 +3,7 @@ using CarNotes.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 
 namespace CarNotes.Classes
@@ -78,6 +79,102 @@ namespace CarNotes.Classes
                 .Select(e=>new BarChartModel{ Date = e.Key.ToString("dd.MM.yyyy"), Cost = e.Value } )
                 .ToList();
             return result;
+        }
+        /// <summary>
+        /// получение общей информации о пользователе для вывода в таблицу 
+        /// </summary>
+        /// <param name="vehicleId"></param>
+        /// <returns></returns>
+        public CommonInformationModel GetDataForCommonInformation(int vehicleId)
+        {
+            var database = new CnDbContext();
+            var commonInformation = new CommonInformationModel();
+            var commonMileageList = database.RefuelEvents.Where(e => e.VehicleId == vehicleId).Select(e => e.Mileage)
+                .Union(database.RepairEvents.Where(e => e.VehicleId == vehicleId).Select(e => e.Mileage))
+                .Union(database.Expenses.Where(e => e.Mileage != null && e.VehicleId == vehicleId).Select(e => (double)e.Mileage))
+                .GroupBy(e => true, (e, records) => new
+                {
+                    maximum = records.Max(),
+                    minimum = records.Min()
+                })
+                .ToList();
+            commonInformation.CommonMileage = (int)(commonMileageList[0].maximum - commonMileageList[0].minimum);
+            var oldestDate = database.RefuelEvents.Where(e => e.VehicleId == vehicleId).Select(e => e.Date)
+                .Union(database.RepairEvents.Where(e => e.VehicleId == vehicleId).Select(e => e.Date))
+                .Union(database.Expenses.Where(e => e.VehicleId == vehicleId).Select(e => e.Date))
+                .Min(e => e);
+            commonInformation.TotalTime = GetTimeSpentOnSite(oldestDate);
+            commonInformation.RefuelAmount = database.RefuelEvents.Where(e => e.VehicleId == vehicleId).Count();
+            commonInformation.RepairAmount = database.RepairEvents.Where(e => e.VehicleId == vehicleId).Count();
+            commonInformation.ExpenseAmount = database.Expenses.Where(e => e.VehicleId == vehicleId).Count();
+            commonInformation.RefuelCost = (int)database.RefuelEvents
+                .Where(e => e.VehicleId == vehicleId)
+                .Select(e => e.Volume * e.PricePerOneLiter)
+                .Sum(e => e);
+            commonInformation.RepairCost = (int)database.RepairEvents
+                .Include(x => x.Parts)
+                .Where(e => e.VehicleId == vehicleId)
+                .Select(e => (double)e.RepairCost + e.Parts.Sum(p => p.Price))
+                .Sum();
+            commonInformation.ExpenseCost = (int)database.Expenses
+                .Where(e => e.VehicleId == vehicleId)
+                .Select(e => (double)e.Sum)
+                .Sum(e => e);
+            commonInformation.AverageFuelPrice = (int)database.RefuelEvents.Where(e=>e.VehicleId == vehicleId)
+                .Average(e=>e.PricePerOneLiter);            
+            return commonInformation;
+        }
+        /// <summary>
+        /// вычисление количества лет, месяцев и дней на сайте относительно текущего момента
+        /// </summary>
+        /// <param name="timeStart"></param>дата записи первого события на сайте
+        public CommonTimeOnSite GetTimeSpentOnSite(DateTime timeStart)
+        { 
+            var timeSpent = new CommonTimeOnSite();
+            timeSpent.Year = DateTime.Now.Year - timeStart.Year;
+            timeSpent.Month = DateTime.Now.Month - timeStart.Month;
+            timeSpent.Day = DateTime.Now.Day - timeStart.Day;
+            timeSpent.formYear = GetWordForm(timeSpent.Year, "year");
+            timeSpent.formMonth = GetWordForm(timeSpent.Month, "month");
+            timeSpent.formDay = GetWordForm(timeSpent.Day, "day");
+            return timeSpent;
+        }
+        /// <summary>
+        /// получение правильной формы слова для обозначения временного участка (год, месяц, день) в зависимости от численного значения этого участка
+        /// </summary>
+        /// <param name="amount"></param> количество временных участков (год, месяц, день)
+        /// <param name="timeForm"></param> название передаваемого временного участка (year, month, day)
+        /// <returns></returns>
+        public string GetWordForm(int amount, string timeForm)
+        {
+            if ((11 <= amount % 100 && amount % 100 <= 14) || amount % 10 >= 5)
+            {
+                switch (timeForm)
+                {
+                    case "year": return "лет";
+                    case "month": return "месяцев";
+                    case "day": return "дней";
+                }
+            }
+            else if (amount % 10 == 1)
+            {
+                switch (timeForm)
+                {
+                    case "year": return "год";
+                    case "month": return "месяц";
+                    case "day": return "день";
+                }
+            }
+            else if (2 <= amount % 10 && amount % 10 <= 4)
+            {
+                switch (timeForm)
+                {
+                    case "year": return "годa";
+                    case "month": return "месяца";
+                    case "day": return "дня";
+                }
+            }
+            return "";
         }
     }
 }

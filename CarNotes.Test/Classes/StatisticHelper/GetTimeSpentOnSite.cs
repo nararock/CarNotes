98a18d84c51f44d9ac5ff8ec312e6d583181276c
Elena Gerasimovich <nararock@mail.ru>
2022-09-12 01:12:24 +0300

    Добавлены тесты для проверки метода GetTimeSpentOnSite в классе StatisticHelper.

---
+++ CarNotes.Test/Classes/StatisticHelper/GetTimeSpentOnSite.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarNotes.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarNotes.Test.Classes.StatisticHelper
+{
+    [TestClass]
+    public class GetTimeSpentOnSite
+    {
+        [TestMethod]
+        public void Yesterday()
+        {
+            var statisticHelper = new CarNotes.Classes.StatisticHelper();
+            var start = new DateTime(2000, 01, 01, 0, 0, 0);
+            var end = new DateTime(2000, 01, 02, 0, 0, 0);
+            var reference = new CommonTimeOnSite
+            {
+                Day = 1,
+                formDay = "день",
+                formMonth = "",
+                formYear = ""
+            };
+            var result = statisticHelper.GetTimeSpentOnSite(start, end);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(reference.Day, result.Day);
+            Assert.AreEqual(reference.Month, result.Month);
+            Assert.AreEqual(reference.Year, result.Year);
+            Assert.AreEqual(reference.formDay, result.formDay);
+            Assert.AreEqual(reference.formMonth, result.formMonth);
+            Assert.AreEqual(reference.formYear, result.formYear);
+        }
+
+        [TestMethod]
+        public void MonthWithoutDay()
+        {
+            var statisticHelper = new CarNotes.Classes.StatisticHelper();
+            var start = new DateTime(2000, 01, 03, 0, 0, 0);
+            var end = new DateTime(2000, 02, 02, 0, 0, 0);
+            var reference = new CommonTimeOnSite
+            {
+                Day = 30,
+                formDay = "дней",
+                formMonth = "",
+                formYear = ""
+            };
+            var result = statisticHelper.GetTimeSpentOnSite(start, end);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(reference.Day, result.Day);
+            Assert.AreEqual(reference.Month, result.Month);
+            Assert.AreEqual(reference.Year, result.Year);
+            Assert.AreEqual(reference.formDay, result.formDay);
+            Assert.AreEqual(reference.formMonth, result.formMonth);
+            Assert.AreEqual(reference.formYear, result.formYear);
+        }
+
+        [TestMethod]
+        public void NumberMonthsIsZero()
+        {
+            var statisticHelper = new CarNotes.Classes.StatisticHelper();
+            var start = new DateTime(2001, 01, 05, 0, 0, 0);
+            var end = new DateTime(2002, 01, 03, 0, 0, 0);
+            var reference = new CommonTimeOnSite()
+            {
+                Day = 28,
+                Month = 11,
+                formDay = "дней",
+                formMonth = "месяцев",
+                formYear = ""
+            };
+            var result = statisticHelper.GetTimeSpentOnSite(start, end);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(reference.Day, result.Day);
+            Assert.AreEqual(reference.Month, result.Month);
+            Assert.AreEqual(reference.Year, result.Year);
+            Assert.AreEqual(reference.formDay, result.formDay);
+            Assert.AreEqual(reference.formMonth, result.formMonth);
+            Assert.AreEqual(reference.formYear, result.formYear);
+        }
+    }
+}
---
+++ CarNotes.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarNotes.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarNotes.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3293bec4-e96e-4850-a270-0fd123266073")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

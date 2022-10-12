de5917e4f42e8b2afe942916523f736bcf873479
Andrew Gerasimovich <byte916@yandex.ru>
2022-10-12 17:21:10 +0300

    Исправлена ошибка в предыдущем коммите

--- CarNotes/Controllers/LoginController.cs
+++ CarNotes/Controllers/LoginController.cs
@@ -14,6 +14,7 @@ namespace CarNotes.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            ViewBag.IsNewRegistration = false;
             return View();
         }
 

15f3113d114d24b6a56edf4e75b23191ff7068b7
Elena Gerisimovich <nararock@mail.ru>
2025-01-20 18:59:11 +0300

    Добавлен Angular (для реализации личного кабинета), стр и контроллер Account.

---
+++ CarNotes/src/app/menu-component/menu-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-component',
+  imports: [],
+  templateUrl: './menu-component.component.html',
+  styleUrl: './menu-component.component.css'
+})
+export class MenuComponentComponent {
+
+}
